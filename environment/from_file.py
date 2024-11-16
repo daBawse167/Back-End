b696f76e105a044e614c677c3c8019ef4cb67bf5
Thomas Henderson <93086896+Hende2@users.noreply.github.com>
2024-11-16 15:07:04 +0000

    Created the functions to make python objects out of the data. Next step: Configure database in relational fashion.

--- environment/from_file.py
+++ environment/from_file.py
@@ -1,5 +1,29 @@
 import json
 
+class identifiers:
+    def __init__(self, id, datetime, hasSynced, isSafe, location, uid):
+        self.id = id
+        self.datetime = datetime
+        self.hasSynced = hasSynced
+        self.isSafe = isSafe
+        self.location = location
+        self.uid = uid
+
+    def __repr__(self):
+        return f"Identifiers({self.id}, {self.datetime}, {self.hasSynced}, {self.isSafe}, {self.location}, {self.uid})"
+
+class measurements:
+    def __init__(self, id, chloride, ph, temperature, turbidity, timeIntervals):
+        self.id = id
+        self.chloride = chloride
+        self.ph = ph
+        self.temperature = temperature
+        self.turbidity = turbidity
+        self.timeIntervals = timeIntervals
+
+    def __repr__(self):
+        return f"Measurements({self.id}, {self.chloride}, {self.ph}, {self.temperature}, {self.turbidity}, {self.timeIntervals})"
+
 def remove_comments(jsonc):
     ''' Comments are the only differentiating factor between jsonc and json, so create a list representing the jsonc file and remove all 
     lines starting with the // denoting a comment'''
@@ -18,5 +42,13 @@ def jsonc_to_json_func(jsonc_file):
     parsed_json = json.loads(json_content)
     return parsed_json
     
+def unpackage(dict):
+    '''Converts the dictionary obtained from the json file into two python objects that satisfy relational requirements'''
+    identifier_obj = identifiers(dict['id'], dict['datetime'], dict['hasSynced'], dict['isSafe'], dict['location'], dict['uid'])
+    data_obj = measurements(dict['id'], dict['measurements'][0]['values'], dict['measurements'][1]['values'], dict['measurements'][2]['values'], dict['measurements'][3]['values'], dict['measurements'][1]['values'])
+    print(identifier_obj)
+    print(data_obj)
+    return identifier_obj, data_obj
 
 print(jsonc_to_json_func('readings_data.jsonc'))
+unpackage(jsonc_to_json_func('readings_data.jsonc'))
